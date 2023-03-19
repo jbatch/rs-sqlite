989f26410d6552e285a5aba85c3e2ceadebf3f2e
Josh Batchelor <josh_batch@live.com.au>
2023-03-19 10:16:26 +0800

    Refactor to not use mutable iter

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ pub enum PageType {
 }
 
 impl PageType {
-    pub fn new(byte: u8) -> Result<PageType> {
+    pub fn new(byte: &u8) -> Result<PageType> {
         match byte {
             0x0d => Ok(PageType::TableLeafPage),
             0x0a => Ok(PageType::IndexLeafPage),
@@ -31,9 +31,49 @@ pub struct PageHeader {
     pub number_fragmented_bytes: u8,
 }
 
+enum SqliteSchemaRecordType {
+    TABLE,
+    INDEX,
+    VIEW,
+    TRIGGER,
+}
+
+impl SqliteSchemaRecordType {
+    pub fn new(s: &str) -> Result<SqliteSchemaRecordType> {
+        match s {
+            "table" => Ok(SqliteSchemaRecordType::TABLE),
+            "index" => Ok(SqliteSchemaRecordType::INDEX),
+            "view" => Ok(SqliteSchemaRecordType::VIEW),
+            "trigger" => Ok(SqliteSchemaRecordType::TRIGGER),
+            _ => bail!(format!("Invalid SqliteSchemaRecordType string")),
+        }
+    }
+}
+
+pub struct SqliteSchemaRecord {
+    record_type: SqliteSchemaRecordType,
+    name: String,
+    table_name: String,
+    root_page: u8,
+    sql: String,
+}
+pub struct SqliteSchemaTable {
+    rows: Vec<SqliteSchemaRecord>,
+}
+
+impl SqliteSchemaTable {
+    fn new(bytes: &mut IntoIter<u8>) -> Result<SqliteSchemaTable> {
+        Ok(SqliteSchemaTable { rows: vec![] })
+    }
+}
+
 pub struct Cell {}
 
-impl Cell {}
+impl Cell {
+    fn new(bytes: &Vec<u8>, cell_pointer: &u16) -> Result<Cell> {
+        Ok(Cell {})
+    }
+}
 
 pub struct Page {
     pub page_header: PageHeader,
@@ -42,20 +82,48 @@ pub struct Page {
 }
 
 impl Page {
-    pub fn new(bytes: &mut IntoIter<u8>) -> Result<Page> {
-        let page_type = PageType::new(bytes.next().unwrap())?;
-        let freeblock = u16::from_be_bytes(bytes.take(2).collect::<Vec<u8>>().try_into().unwrap());
-        let number_cells =
-            u16::from_be_bytes(bytes.take(2).collect::<Vec<u8>>().try_into().unwrap());
-        let content_start_area =
-            u16::from_be_bytes(bytes.take(2).collect::<Vec<u8>>().try_into().unwrap());
+    pub fn new(bytes: &Vec<u8>) -> Result<Page> {
+        let mut index = 0;
+        let page_type = PageType::new(bytes.get(index).unwrap())?;
+        index += 1;
+
+        let freeblock_bytes = vec![index, index + 1]
+            .iter()
+            .map(|v| bytes[*v])
+            .collect::<Vec<u8>>();
+        index += 2;
+        let freeblock = u16::from_be_bytes(freeblock_bytes.try_into().unwrap());
+
+        let number_cells_bytes = vec![index, index + 1]
+            .iter()
+            .map(|v| bytes[*v])
+            .collect::<Vec<u8>>();
+        index += 2;
+        let number_cells = u16::from_be_bytes(number_cells_bytes.try_into().unwrap());
+
+        let content_start_area_bytes = vec![index, index + 1]
+            .iter()
+            .map(|v| bytes[*v])
+            .collect::<Vec<u8>>();
+        index += 2;
+        let content_start_area = u16::from_be_bytes(content_start_area_bytes.try_into().unwrap());
+
+        let number_fragmented_bytes_bytes =
+            vec![index].iter().map(|v| bytes[*v]).collect::<Vec<u8>>();
+        index += 1;
         let number_fragmented_bytes =
-            u8::from_be_bytes(bytes.take(1).collect::<Vec<u8>>().try_into().unwrap());
-        let cell_pointers: Vec<u16> = bytes
-            .take(number_cells as usize * 2)
+            u8::from_be_bytes(number_fragmented_bytes_bytes.try_into().unwrap());
+
+        let cell_pointers = (index..(index + number_cells as usize * 2))
+            .map(|v| bytes[v])
             .collect::<Vec<u8>>()
             .chunks(2)
             .map(|chunk| u16::from_be_bytes([chunk[0], chunk[1]]))
+            .collect::<Vec<u16>>();
+
+        let cells: Vec<Cell> = cell_pointers
+            .iter()
+            .map(|cell_pointer| Cell::new(bytes, cell_pointer).unwrap())
             .collect();
         Ok(Page {
             page_header: PageHeader {
@@ -92,7 +160,7 @@ fn main() -> Result<()> {
             #[allow(unused_variables)]
             let page_size: u16 = u16::from_be_bytes([header[16], header[17]]);
 
-            let mut root_bytes = vec![0u8; page_size as usize];
+            let mut root_bytes = vec![0u8; (page_size - 100) as usize];
             file.read_exact_at(&mut root_bytes, 100)?;
             // println!("root {:x?}", root);
             // println!(
@@ -102,7 +170,7 @@ fn main() -> Result<()> {
             //     root[3779 + 1],
             //     root[3779 + 4]
             // );
-            let page = Page::new(&mut root_bytes.into_iter())?;
+            let page = Page::new(&root_bytes)?;
 
             println!("database page size: {}", page_size);
             println!("number of tables: {}", page.cell_pointers.len())
